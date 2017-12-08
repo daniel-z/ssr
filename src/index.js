cd2317eedc3c229e97795df55360a5f5fe5a1858
Daniel Zamorano <dzamorano@tacitknowledge.com>
2017-12-07 18:48:43 -0600

    Adding redux stores for client and server.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import Home from './client/components/home';
 import renderer from './helpers/renderer';
+import createStore from "./helpers/createStore";
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    res.send(renderer(req));
+    const store = createStore();
+
+    // some logic to initialize the store
+    // and load data into the store
+
+    res.send(renderer(req, store));
 });
 
 app.listen(3000, () => {
