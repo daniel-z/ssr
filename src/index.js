cbd41f556428d373ad80f3b8828aa5e54e890db6
Daniel Zamorano <dzamorano@tacitknowledge.com>
2017-12-08 20:38:33 -0600

    Adding proxy for authentication.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ import renderer from './helpers/renderer';
 import createStore from "./helpers/createStore";
 import Routes from './client/Routes';
 import { matchRoutes } from 'react-router-config';
-
+import proxy from 'express-http-proxy';
 const app = express();
 
 app.use(express.static('public'));
+
+app.use('/api', proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(opts) {
+        opts.header['x-forwarded-host'] =  'localhost:3000';
+        return opts;
+    }
+}));
 
 app.get('*', (req, res) => {
     const store = createStore();
