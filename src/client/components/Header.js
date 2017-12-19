cf6e549472048e457aba913debb93c0b46a13594
Daniel Zamorano <dzamorano@tacitknowledge.com>
2017-12-18 19:22:13 -0600

    Adding login/logout button

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -3,11 +3,22 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 const Header = ({ auth }) => {
-return (
+  console.log(auth);
+  const authButton = auth ? (
+    <a href="/api/logout"> Logout </a>
+  ): (
+    <a href="/api/auth/google"> Login </a>
+  );
+  return (
     <div>
       <h1>Header</h1>
       <nav>
         <Link to="/">Home</Link>
+        <div>
+          <Link to="/users">Users</Link>
+          <Link to="/admin">Admins</Link>
+          { authButton }
+        </div>
       </nav>
     </div>
   )
